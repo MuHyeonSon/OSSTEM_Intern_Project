4dcc38c3cee004597a551218d87b04f297606f4d
Sonmoohyeon <smuhyeon84@gmail.com>
2022-08-07 10:15:34 +0900

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,15 @@
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include "mainwindow.h"     // mainwindow.h 메인 윈도우 헤더파일 include
+#include "ui_mainwindow.h" //??
 
 MainWindow::MainWindow(QWidget *parent) // constructor(생성자)
     : QMainWindow(parent) //
-    , ui(new Ui::MainWindow)
+    , ui(new Ui::MainWindow) //
 {
-    ui->setupUi(this);
+    ui->setupUi(this); //
 }
 
-MainWindow::~MainWindow()
+MainWindow::~MainWindow() //
 {
-    delete ui;
+    delete ui; //
 }
 
